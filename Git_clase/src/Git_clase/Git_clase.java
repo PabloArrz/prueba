4e76ede7cdd14a96ff3b7c60089612e3d5bd1bac
user.email <Mati@2-12-PC-10>
2020-02-06 12:36:33 +0100

    Versión actualizada.
    Signed-off-by: user.email <Mati@2-12-PC-10>

--- Git_clase/src/Git_clase/Git_clase.java
+++ Git_clase/src/Git_clase/Git_clase.java
@@ -6,6 +6,9 @@ public class Git_clase {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		System.out.println("Holaa");
+		System.out.println("Mundoo");
+		System.out.println("Que talll");
 	}
 
 
